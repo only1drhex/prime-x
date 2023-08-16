3237a4194786ad825bbc2fc6cab4f3c0fd2dad52
only1drhex <lordrhexx@gmail.com>
2023-08-16 22:06:50 +0100

    handle errors

--- src/prime.js
+++ src/prime.js
@@ -1,17 +1,10 @@
 function checkPrime(num) {
-  
-  switch(num){
-    // 0 and 1 are not prime
-    case 0:
-    case 1:
-      return false;
-    // 2 and 3 are prime
-    case 2:
-    case 3:
-      return true;
-  }
-  // Even numbers except 2 are not prime numbers
- 
+   num = parseInt(num);
+  if(!num) return false;
+// numbers less than 2 aren't prime numbers.
+  if (num < 2) return false;
+  if (num === 2) return true;
+// Even numbers (except 2) are not prime numbers.
  if (num % 2 === 0) return false;
 
   let i = 3;
@@ -30,14 +23,9 @@ function computePrime(lower, upper) {
   if (lower === undefined) lower = 0;
   if(!upper) return [];
   for (let i = lower; i <= upper; i++) {
-/* 
-/* Unecessary
-if (primeNumbers.length >= 1) {
-      const lastIndex = primeNumbers.length - 1;
-      const lastNum = primeNumbers[lastIndex];
-      if (lastNum == upper) return primeNumbers;
-    }*/
+
     if (checkPrime(i)) primeNumbers.push(i);
+ 
   }
   return primeNumbers;
 }
